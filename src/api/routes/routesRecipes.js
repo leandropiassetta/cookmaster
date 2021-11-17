7c74b4b34916f102668563bf65f70cbac8334170
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-11-17 13:56:27 -0300

    deploy

--- src/api/routes/routesRecipes.js
+++ src/api/routes/routesRecipes.js
@@ -3,11 +3,11 @@ const controllersRecipes = require('../../controllers/recipesControllers');
 const middlewareAuth = require('../../middlewares/authentication');
 const upload = require('../../middlewares/uploads');
 
-router.get('/', controllersRecipes.getAll);
-router.get('/:id', controllersRecipes.getById);
-router.post('/', middlewareAuth, controllersRecipes.createRecipe);
-router.put('/:id', middlewareAuth, controllersRecipes.editRecipe);
 router.put('/:id/image/', middlewareAuth, upload.single('image'), controllersRecipes.upload);
+router.get('/:id', controllersRecipes.getById);
+router.put('/:id', middlewareAuth, controllersRecipes.editRecipe);
 router.delete('/:id', middlewareAuth, controllersRecipes.deleteRecipe);
+router.get('/', controllersRecipes.getAll);
+router.post('/', middlewareAuth, controllersRecipes.createRecipe);
 
 module.exports = router;
